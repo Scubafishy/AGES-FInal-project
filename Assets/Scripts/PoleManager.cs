dbe2ec9f2f801cc1aa46d3918a2e324f89b98cc4
Scubafishy <32077848+Scubafishy@users.noreply.github.com>
2018-04-16 10:00:20 -0500

    inital

---
+++ Assets/Scripts/PoleManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoleManager : MonoBehaviour
+{
+
+    public bool Clicked = false;
+
+    public  bool canBeClicked = false;
+
+    private bool alreadyClciked = false;
+
+    public LayerMask Poles;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Collider[] colliders=Physics.OverlapSphere(transform.position, Poles);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody targetRidgidbody = colliders[i].GetComponent<Rigidbody>();
+            if (!targetRidgidbody)
+                continue;
+
+            PoleManager inRangePole = targetRidgidbody.GetComponent<PoleManager>();
+
+            if (!inRangePole)
+                continue;
+
+            inRangePole.Acivate();
+        }
+    }
+
+    public void Acivate()
+    {
+        canBeClicked = true;
+    }
+
+    private void OnMouseDown()
+    {
+        if (canBeClicked == true && alreadyClciked == false)
+        {
+            Debug.Log("I was clicked");
+        }
+
+       if(canBeClicked == false)
+        {
+            Debug.Log("im out of range");
+        }
+        
+    }
+}
---
+++ Assets/Scripts/WireManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Pole1;
+
+    [SerializeField]
+    GameObject Pole2;
+
+    private Renderer myRender;
+
+    // Use this for initialization
+    void Start ()
+    {
+         myRender = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        activateWire();
+    }
+
+    private void activateWire()
+    {
+        if(Pole1.GetComponent<PoleManager>().Clicked ==true && Pole2.GetComponent<PoleManager>().Clicked == true)
+        {
+            myRender.enabled = false;
+        }
+    }
+}
